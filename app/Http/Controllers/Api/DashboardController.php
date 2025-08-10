95b96e0c1baa222d4dd3edd2db05dd8ac9f82a66
LUTHFI BAEHAQI <117705394+baehaqi312@users.noreply.github.com>
2025-08-10 22:22:36 +0700

    Update Dashboard Controller

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -10,18 +10,25 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $user = $request->user();
+
         $users = User::count();
         $products = Product::count();
         $transactions = Order::count();
         $transactionsAdmin = Order::where('payment_status', 'success')->sum('total_price');
+
+        $order = Order::where('user_id', $user->id)->count();
+        $orderSuccess = Order::where('user_id', $user->id)->where('payment_status', 'success')->count();
 
         return response()->json([
             'users' => $users,
             'products' => $products,
             'transactions' => $transactions,
             'transactions_admin' => $transactionsAdmin,
+            'order_perngguna' => $order,
+            'order_success' => $orderSuccess
         ]);
     }
 }
